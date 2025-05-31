2b3d93f8a9924c5e89a145a2e4fb4c147013fb73
CavannaSoftware <cavanna.software@gmail.com>
2025-05-31 08:57:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -577,7 +577,7 @@ st.markdown("""
 st.markdown(
     """
     <div class='center-logo'>
-        <img src='https://raw.githubusercontent.com/CavannaSoftware/LogisticsOS/refs/heads/main/logo.png' width='150'>
+        <img src='https://raw.githubusercontent.com/CavannaSoftware/LogisticsOS/refs/heads/main/logo.png' width='500'>
     </div>
     """,
     unsafe_allow_html=True
